12ab0b3957276d8a232787103f730b9fc6eb5e01
izowo <62592437+izowo@users.noreply.github.com>
2020-11-16 17:23:22 +0900

    Add files via upload

---
+++ aka.js
@@ -0,0 +1,135 @@
+
+var cs       = document.getElementById('myCanvas'),
+    ctx      = cs.getContext('2d'),
+    csWidth  = cs.width,
+    csHeight = cs.height,
+    center   = {
+      x: csWidth / 2,
+      y: csHeight / 2
+    };
+
+// 線の共通スタイル
+ctx.strokeStyle = '#000';
+ctx.lineWidth   = 3;
+ctx.lineJoin    = 'round';
+
+
+var Graph = function(arg) {
+  this.initialize(arg);
+  /* 共通の静的プロパティ */
+  this.moveLength = 0;
+  this.addLength  = 20;
+  this.isAnim     = function() {
+    return (this.moveLength < this.hypotenuse);
+  };
+};
+
+(function (p) {
+  /**
+   * インスタンスごとの初期設定
+   * @param  {array.obj} arg [beginPos.x, beginPos.y, endPos.x, endPos.y]
+   */
+  p.initialize = function(arg) {
+    this.dfd = $.Deferred();
+    // 左右に線が引かれるので位置に注意
+    this.beginPos = {
+      x: arg.beginPos.x,
+      y: arg.beginPos.y
+    };
+    this.movePos = {
+      x: arg.beginPos.x,
+      y: arg.beginPos.y
+    };
+    this.endPos = {
+      x: arg.endPos.x,
+      y: arg.endPos.y
+    };
+    this.side = {
+      x: this.endPos.x - this.beginPos.x,
+      y: this.endPos.y - this.beginPos.y
+    };
+    this.hypotenuse = Math.sqrt(Math.pow(this.side.x, 2) + Math.pow(this.side.y, 2));
+    this.radian = Math.atan2(this.side.y, this.side.x);
+  };
+  p.draw = function() {
+    ctx.beginPath();
+    ctx.moveTo(this.beginPos.x, this.beginPos.y);
+    ctx.lineTo(this.movePos.x, this.movePos.y);
+    ctx.closePath();
+    ctx.stroke();
+  };
+  p.update = function() {
+    this.moveLength += this.addLength;
+    this.movePos.x += Math.cos(this.radian) * this.addLength;
+    this.movePos.y += Math.sin(this.radian) * this.addLength;
+  };
+  p.render = function() {
+    this.draw();
+    if (this.isAnim() === true) {
+      this.update();
+      this.movePos.x = (this.isAnim() === false) ? this.endPos.x : this.movePos.x;
+      this.movePos.y = (this.isAnim() === false) ? this.endPos.y : this.movePos.y;
+      requestAnimationFrame(this.render.bind(this));
+    } else {
+      // アニメーションが完了したら通知する
+      this.dfd.resolve();
+    }
+    return this.dfd.promise();
+  };
+})(Graph.prototype);
+
+
+
+
+
+// 以降、グラフインスタンスの生成とレンダリング
+
+
+function Add(){
+ document.fm.C.value = document.fm.A.value + document.fm.B.value;
+
+
+
+var graphData = [
+  {
+    beginPos: { x: csWidth/2, y: 0},
+    endPos:   { x: csWidth, y: csWidth/2}
+  },
+  {
+    beginPos: { x: csWidth, y: csWidth/2},
+    endPos:   { x: csWidth/2, y: csWidth}
+  },
+  {
+    beginPos: { x: csWidth/2, y: csWidth},
+    endPos:   { x: 0, y: csWidth/2}
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+var graphObj = {},
+    i = 0,
+    j = 0,
+    l = graphData.length;
+for (; i < l; i++) {
+  graphObj[i] = new Graph(graphData[i]);
+}
+
+var d = (new $.Deferred()).resolve();
+$.each(graphObj, function(i, obj){
+  d = d.then(function() {
+    return obj.render();
+  });
+});
+
+}
